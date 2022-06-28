a773a1081cf244172072527c2b1ba352a278f67e
Hershell Koger <hersheykoger@yahoo.com>
2022-06-28 08:38:17 -0400

    Add DOM bindings and event listener for button functionality

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+const display = document.getElementById('display')
+
+const btn = document.querySelectorAll('button');
+btn.forEach(button => {
+    button.addEventListener('click', () => {
+        display.textContent = button.id;
+    })
+});
